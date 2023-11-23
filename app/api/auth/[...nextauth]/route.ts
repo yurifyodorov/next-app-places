7a39b66ee5076f9fca19e99fcdef8cbdf65fb850
Yuri Fedorov <yuri.inpost@gmail.com>
2023-11-23 17:45:15 +0300

    NEXTJS-418: update Auth API

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import prisma from "@/app/libs/prismadb";
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthOptions  = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
