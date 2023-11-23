8f468d009ed9bc030b44cf9fd4ef8a3a3a825ae0
Yuri Fedorov <yuri.inpost@gmail.com>
2023-11-23 18:22:32 +0300

    NEXTJS-418: update getCurrentUser action

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,10 +1,9 @@
 import { getServerSession } from "next-auth/next"
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
-  return await getServerSession(authOptions)
+  return await getServerSession()
 }
 
 export default async function getCurrentUser() {
