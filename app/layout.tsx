66299337f256b07afc8fe1f475076fa86c83a863
Yuri Fedorov <yuri.inpost@gmail.com>
2023-11-23 15:46:20 +0300

    NEXTJS-418: Filters modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/app/components/navbar/Navbar";
 import LoginModal from "@/app/components/modals/LoginModal";
 import RegisterModal from "@/app/components/modals/RegisterModal";
 import RentModal from "@/app/components/modals/RentModal";
+import SearchModal from "@/app/components/modals/SearchModal";
 
 import ToasterProvider from "@/app/providers/ToasterProvider";
 
@@ -35,6 +36,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
+        <SearchModal />
         <RentModal />
         <Navbar currentUser={currentUser} />
       </ClientOnly>
